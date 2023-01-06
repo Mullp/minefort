def3c8304e5590151a261f1854e76effe661c365
Mullp <mullp@protonmail.com>
2023-01-06 21:10:45 +0100

    Fixed auth

--- src/managers/AuthManager.ts
+++ src/managers/AuthManager.ts
@@ -8,12 +8,43 @@ export class AuthManager extends BaseManager {
     super(client);
   }
 
-  public async auth(email: string, password: string): Promise<void> {
-    await fetch(this.client.BASE_URL + '/auth/login', {
+  public async authenticate(email: string, password: string): Promise<boolean> {
+    return await fetch(this.client.BASE_URL + '/auth/login', {
       method: 'POST',
-      body: `{"emailAddress":"${email}","password":"${password}"}`,
+      body: JSON.stringify({
+        emailAddress: email,
+        password: password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
     })
-      .then(res => res.json() as Promise<AuthResponse>)
-      .then(value => {});
+      .then(res => {
+        return Promise.all([
+          res.json() as Promise<AuthResponse>,
+          res.headers
+            .get('set-cookie')
+            ?.split('; ')[0]
+            .replace('minefort-session=', ''),
+        ]);
+      })
+      .then(value => {
+        if (value[0].status === AuthStatus.OK) {
+          if (value[1]) {
+            this.client.cookie = value[1];
+          }
+          return true;
+        } else if (value[0].status === AuthStatus.INVALID_CREDENTIALS) {
+          throw new Error('Invalid credentials');
+        } else if (value[0].status === AuthStatus.INVALID_INPUT) {
+          throw new Error('Invalid input: ' + value[0].error.body[0].message);
+        }
+
+        return false;
+      })
+      .catch(error => {
+        throw error;
+      });
   }
 }
